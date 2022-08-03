2e0e0b5ced56aaeab8ab06a44384813f1cd050a0
Eduardo Moura <79063539+EduMoura-debug@users.noreply.github.com>
2022-08-03 18:41:17 -0300

    Add files via upload

---
+++ paraboloide.py
@@ -0,0 +1,129 @@
+import sys
+import sdl2
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from math import *
+import math
+
+
+def InitGL(Width, Height):             
+    glClearColor(0.0, 0.0, 0.0, 0.0) 
+    glClearDepth(1.0)
+    glDepthFunc(GL_LESS)               
+    glEnable(GL_DEPTH_TEST)            
+    glShadeModel(GL_SMOOTH)            
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+    glMatrixMode(GL_MODELVIEW)
+
+    #LUZ!!!
+    mat_ambient = (0.4, 0.0, 0.0, 1.0)
+    mat_diffuse = (1.0, 0.0, 0.0, 1.0)
+    mat_specular = (1.0, 0.5, 0.5, 1.0)
+    mat_shininess = (50,)
+    light_position = (0, 10, 10)
+    glShadeModel(GL_SMOOTH)
+    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient)
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse)
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_LIGHT0)
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position)
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_MULTISAMPLE)
+    gluLookAt(0,0,10,0,0,0,0,1,0)
+
+def calculaNormal(v0,v1,v2):
+    x = 0
+    y = 1
+    z = 2
+    U = ( v2[x]-v0[x], v2[y]-v0[y], v2[z]-v0[z] )
+    V = ( v1[x]-v0[x], v1[y]-v0[y], v1[z]-v0[z] )
+    N = ( (U[y]*V[z]-U[z]*V[y]),(U[z]*V[x]-U[x]*V[z]),(U[x]*V[y]-U[y]*V[x]))
+    NLength = sqrt(N[x]*N[x]+N[y]*N[y]+N[z]*N[z])
+    return ( N[x]/NLength, N[y]/NLength, N[z]/NLength)
+
+def map(valor, v0, vf, m0, mf):
+    return m0+(((valor-v0)*(mf-m0))/(vf-v0))
+ 
+def cor(i,j):
+    theta = map(i,0,N,-math.pi/2,math.pi/2)
+    phy = map(j,0,N,0,2*math.pi)
+    r = 0.5+0.5*math.sin(theta)
+    g = 0.5+0.5*math.cos(phy)
+    b = r
+    return r, g, b
+
+def f(x,y):
+	return x**2 - y**2   #Mudar pra +
+
+N = 20
+a=0
+r=1
+x0=-2
+xf=2
+y0=-2
+yf=2
+dx= (xf-x0)/(N)
+dy= (yf-y0)/(N)
+
+#def emiteVertex(x,y):
+    
+
+def desenha():
+    global a
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)    
+    glLoadIdentity()       
+    glTranslatef(0.0,0.0,-20.0)
+    glRotatef(a,0.0,1.0,0.0)
+    y=y0      
+    for i in range(0,N):
+        glBegin(GL_TRIANGLE_STRIP)
+        x=x0
+        for j in range(0,N+1):   
+            z1=f(x,y)
+            z2=f(x,y+dy)
+            z3=f(x+dx,y)
+            n = calculaNormal((x,y,z1),(x,y+dy,z2),(x+dx,y,z3))
+            glNormal3fv(n)
+            glColor3f(1,1,0)
+            glVertex3f(x,y,z1)
+            glColor3f(1,1,0)
+            glVertex3f(x,y+dy,z2)
+#            glColor3f(1,1,0)
+#            glVertex3f(x+dx,y,z3)
+
+
+            x += dx   
+        y += dy
+        glEnd()       
+    a+=1
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+sdl2.SDL_Init(sdl2.SDL_INIT_EVERYTHING)
+sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_CONTEXT_MAJOR_VERSION, 2)
+sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_CONTEXT_MINOR_VERSION, 1)
+sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_CONTEXT_PROFILE_MASK,sdl2.SDL_GL_CONTEXT_PROFILE_CORE)
+sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_DOUBLEBUFFER, 1)
+sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_DEPTH_SIZE, 24)
+sdl2.SDL_GL_SetSwapInterval(1)
+window = sdl2.SDL_CreateWindow(b"Paraboloide", sdl2.SDL_WINDOWPOS_CENTERED, sdl2.SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, sdl2.SDL_WINDOW_OPENGL | sdl2.SDL_WINDOW_SHOWN)
+if not window:
+    sys.stderr.write("Error: Could not create window\n")
+    exit(1)
+glcontext = sdl2.SDL_GL_CreateContext(window)
+InitGL(WINDOW_WIDTH,WINDOW_HEIGHT)
+running = True
+event = sdl2.SDL_Event()
+while running:
+    while sdl2.SDL_PollEvent(ctypes.byref(event)) != 0:
+        if event.type == sdl2.SDL_QUIT:
+            running = False
+        if event.type == sdl2.events.SDL_KEYDOWN:
+            if event.key.keysym.sym == sdl2.SDLK_ESCAPE:
+                running = False
+    desenha()
+    sdl2.SDL_GL_SwapWindow(window)
